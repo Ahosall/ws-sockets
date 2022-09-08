8b8c3d006850d67f2ae4bf322c33bfb7c2f07618
Ahosall <zen.wixyos.br@gmail.com>
2022-09-08 18:12:52 -0400

    Adicionado api

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./views/Login";
 import Chat from "./views/Chat";
 
-const socket = io("http://localhost:4000", { transports: ["websocket"] });
+const socket = io("https://pointed-pouncing-puppy.glitch.me", {
+  transports: ["websocket"],
+});
 
 interface IUser {
   name: string;
